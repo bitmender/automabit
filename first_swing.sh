2f8c6f194061031441f1302633fcec9784685abb
Bit Mender <bitmender@gmail.com>
2012-08-09 10:42:38 -0400

    Fix RVM install

--- first_swing.sh
+++ first_swing.sh
@@ -5,75 +5,53 @@ start() {
 
 	RUBY_VER="ruby"
 
-		install_pkg "curl"
-		install_pkg "git"
-		install_rvm
-		rvm_load_function
-		rvm_install_ver $RUBY_VER
-		rvm_use_ver "$RUBY_VER@$HOSTNAME"
-		gemrc_suppress_doc
-		install_thor
-		thor_create_example_task
-		run_example_task
-		thor_install_example_task
-		thor_delete_example_task_file
-                explain_next_steps
+	install_pkg "curl"
+	install_pkg "git"
+	install_rvm
+	rvm_load_function
+	rvm_install_ver $RUBY_VER
+	rvm_use_ver "$RUBY_VER@$HOSTNAME"
+	gemrc_suppress_doc
+	install_thor
+	explain_next_steps
 }
 
 
 
 install_pkg() { 
 	echo "Installing package: $1"
-		sudo apt-get install -y $1
+	sudo apt-get install -y $1
 }
 
 install_rvm() {
 
 	echo "Installing RVM"
-		install_pkg "curl"
-		bash -s stable < <(curl -s https://raw.github.com/wayneeseguin/rvm/master/binscripts/rvm-installer )
-		echo '[[ -s "$HOME/.rvm/scripts/rvm" ]] && . "$HOME/.rvm/scripts/rvm" # Load RVM function' >> ~/.bashrc
-		install_pkg "build-essential openssl libreadline6 libreadline6-dev git-core zlib1g zlib1g-dev libssl-dev libyaml-dev libsqlite3-0 libsqlite3-dev sqlite3 libxml2-dev libxslt-dev autoconf libc6-dev ncurses-dev automake libtool bison subversion"
+	install_pkg "curl"
+	curl -L https://get.rvm.io | bash -s stable
+
+	cat ~/.bash_login >> ~/.profile
+	rm ~/.bash_login
+
+	install_pkg "build-essential openssl libreadline6 libreadline6-dev git-core zlib1g zlib1g-dev libssl-dev libyaml-dev libsqlite3-0 libsqlite3-dev sqlite3 libxml2-dev libxslt-dev autoconf libc6-dev ncurses-dev automake libtool bison subversion"
 
 }
 
 rvm_install_ver() { 
 	echo "Installing RVM Ruby version: $1 "
-		rvm install $1
+	rvm install $1
 }
 
 rvm_use_ver() {
 	echo "Setting up RVM gemset: $1"
-		rvm use --install --create --default $1
-}
-
-
-rvm_load_function() {
-
-	echo "Loading RVM into a shell session *as a function*"
-		if [[ -s "$HOME/.rvm/scripts/rvm" ]] ; then
-
-# First try to load from a user install
-			source "$HOME/.rvm/scripts/rvm"
-
-				elif [[ -s "/usr/local/rvm/scripts/rvm" ]] ; then
-
-# Then try to load from a root install
-				source "/usr/local/rvm/scripts/rvm"
-
-		else
-
-			printf "ERROR: An RVM installation was not found.\n"
-
-				fi
+	rvm use --install --create --default $1
 }
 
 gemrc_suppress_doc() {
 	echo "Disabling rdoc/ri generation in ~/.gemrc"
-		cat >> ~/.gemrc <<EOF
-install: --no-rdoc --no-ri
-update: --no-rdoc --no-ri
-EOF
+	cat >> ~/.gemrc <<EOF
+	install: --no-rdoc --no-ri
+	update: --no-rdoc --no-ri
+	EOF
 }
 
 install_thor() {
@@ -81,57 +59,14 @@ install_thor() {
 	gem install thor
 }
 
-
-
-thor_create_example_task() {
-echo "Creating example test.thor file"
-	cat >> test.thor <<EOF
-class Test < Thor
-  desc "example FILE", "an example task"
-  method_option :delete, :aliases => "-d", :desc => "Delete the file after parsing it"
-  def example(file)
-    puts "You supplied the file: #{file}"
-    delete_file = options[:delete]
-    if delete_file
-      puts "You specified that you would like to delete #{file}"
-    else
-      puts "You do not want to delete #{file}"
-    end
-  end
-end
-EOF
-}
-
-
-run_example_task() {
-
-echo "Running example task"
-thor test:example test.txt
-}
-
-
-
-thor_install_example_task() {
-echo "Installing example task into system tasks (so you can run command from any dir)"
-thor install test.thor --as=test.thor
-
-}
-
-
-thor_delete_example_task_file() {
-echo "Removing unneeded test.thor file created earlier."
-thor uninstall test.thor
-rm test.thor
-}
-
 explain_next_steps() {
-echo "Bootstrap is complete."
-echo "To install automabit's thor files:"
-echo "git clone https://github.com/bitmender/automabit.git"
-echo "cd automabit/tasks"
-echo "thor install <taskname>.thor"
+	echo "Bootstrap is complete."
+	echo "To install automabit's thor files:"
+	echo "git clone https://github.com/bitmender/automabit.git"
+	echo "cd automabit/tasks"
+	echo "thor install <taskname>.thor"
 
-echo "\nOtherwise, just start creating/importing your own thor files."
+	echo "\nOtherwise, just start creating/importing your own thor files."
 }
 
 start
